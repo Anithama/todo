e247b33ecf5aa6f1d0f25e62354583b055d8e54f
Anith-velu <anitha5.sightspectrum@gmail.com>
2024-03-11 12:22:15 +0530

    ll

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TodoWrapper } from "./Component/TodoWrapper";
+
+
+function App() {
+  return (
+    <div>
+      <TodoWrapper/>
+    </div>
+  );
+}
+
+export default App;
